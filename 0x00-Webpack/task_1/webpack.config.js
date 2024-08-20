124fea54dbc092381eac59732b2a54904abd6b2c
Prince Opoku Amaning <princoprince94@gmail.com>
2024-08-20 11:56:26 +0000

    new commit

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: {
-    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
   },
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
 };
