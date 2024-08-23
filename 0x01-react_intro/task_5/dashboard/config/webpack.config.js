42e50f7bc4a4350d0c9cfc39e445b0421b1fbc87
Prince Opoku Amaning <princoprince94@gmail.com>
2024-08-23 18:19:19 +0000

    Create a project using Webpack

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    clean: true,
+  },
+  devServer: {
+    static: path.join(__dirname, "../dist"),
+    compress: true,
+    port: 9000,
+    hot: true,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html",
+    }),
+  ],
+  devtool: "inline-source-map",
+};
---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,4 @@
+import "./styles.css";
+
+const root = document.getElementById("root");
+root.innerHTML = "<h1>Hello, Webpack!</h1>";
