2c7223d5ce757be9d97f0743749c895e27452522
Prince Opoku Amaning <princoprince94@gmail.com>
2024-09-13 22:20:43 +0000

    Converting into an Immutable object using fromJS

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -6,11 +6,3 @@ export default function accessImmutableObject(object, array) {
   return mappedObj.getIn(array, undefined);
 }
 
-const obj = {
-  name: {
-    first: "Guillaume",
-    last: "Salva",
-  },
-};
-
-console.log(accessImmutableObject(obj, ["name", "first"]));
